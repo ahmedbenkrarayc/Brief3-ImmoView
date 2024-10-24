d82025d584f2259b1bdce0be8a43c7ee0b5c7f7c
BoumeshouliYouness <boumeshouliyouness6@gmail.com>
2024-10-24 14:47:07 +0100

    fix js

--- js/slide.js
+++ js/slide.js
@@ -21,6 +21,29 @@ function showSlides(n) {
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+
+ 
+if(slideIndex == 1){
+    slides[slideIndex].style.display = "block";  
+    slides[slides.length-1].style.display = "block";
+    slides[slideIndex+1].style.display = "block";
+}
+else if (slideIndex == slides.length  ){
+    slides[slideIndex-1].style.display = "block";  
+    slides[slideIndex].style.display = "block";
+    slides[0].style.display = "block";
+}else if(slideIndex > 1 && slideIndex < slides.length){
+    slides[slideIndex-1].style.display = "block";  
+    slides[slideIndex].style.display = "block";
+    slides[slideIndex+1].style.display = "block";
+}
+
+//   console.log("slide -1");
+ 
+  console.log("slide ");
+  console.log(slideIndex);
+//   console.log("slide length");
+//   console.log(slides.length);
+  
+//   dots[slideIndex-1].className += " active";
 }
