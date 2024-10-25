ee74dad0ce7056d5701917f78a2c9146b9901e29
BoumeshouliYouness <boumeshouliyouness6@gmail.com>
2024-10-25 23:30:19 +0100

    merge to main

--- js/slide.js
+++ js/slide.js
@@ -66,3 +66,20 @@ function ShowAll(){
     propreties[i].style.display = "block";  
   }
 }
+document.getElementById('button_de_soumettre').addEventListener('click', function() {
+  const username = document.getElementById('username').value;
+  const phone = document.getElementById('phone').value;
+  const email = document.getElementById('email').value;
+  const message = document.getElementById('text_de_message').value;
+
+  if (!username || !phone || !email || !message) {
+      alert('Veuillez remplir tous les champs.');
+      return;
+  }
+ alert("soumettre avec succes")
+});
+
+let image=Document.getElementsById("grand_image1");
+function changerImage(nouvelleImage) {
+  document.getElementById('grand_image1').src = nouvelleImage;
+}
